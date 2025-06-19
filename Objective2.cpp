4f40d2bd7848607117b75865cc1ef0cd6daae1c0
chentx06 <chentx06@gmail.com>
2025-06-19 17:11:22 -0400

    Update Objective2.cpp

--- Objective2.cpp
+++ Objective2.cpp
@@ -1,10 +1,10 @@
 #include "objective2.h"
 #include "Animation/WidgetAnimation.h"
 
+//this is just for the objective to move out into view
 void Uobjective2::NativeConstruct()
 {
     Super::NativeConstruct();
-
     if (NewAnimation)
     {
         PlayAnimation(NewAnimation, 0.f, 1, EUMGSequencePlayMode::Forward, 1.f, false);
