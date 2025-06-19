204619f30727b727ba69fb9263f3e10974fa1a27
chentx06 <chentx06@gmail.com>
2025-06-19 17:01:25 -0400

    Update BackroomsLevel0.h

--- BackroomsLevel0.h
+++ BackroomsLevel0.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "BackroomsLevel0.generated.h"
@@ -8,13 +7,10 @@ UCLASS()
 class YOURGAME_API ABackroomsLevel0 : public AActor
 {
     GENERATED_BODY()
-    
 public:
     ABackroomsLevel0();
-
     UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
     int32 ObjectsCollected;
-
 protected:
     virtual void BeginPlay() override;
 };
