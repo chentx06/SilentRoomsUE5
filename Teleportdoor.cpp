66e801f5e3f4192173614a2eb6917626ef2779a7
chentx06 <chentx06@gmail.com>
2025-06-19 16:45:48 -0400

    Create Teleportdoor.cpp

---
+++ Teleportdoor.cpp
@@ -0,0 +1,30 @@
+#include "teleportdoor.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "BP_FirstPersonCharacter.h"
+
+Ateleportdoor::Ateleportdoor()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    TeleportTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TeleportTrigger"));
+    RootComponent = TeleportTrigger;
+}
+
+void Ateleportdoor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (TeleportTrigger)
+    {
+        TeleportTrigger->OnComponentBeginOverlap.AddDynamic(this, &Ateleportdoor::OnOverlapBegin);
+    }
+}
+
+void Ateleportdoor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (ABP_FirstPersonCharacter* Player = Cast<ABP_FirstPersonCharacter>(OtherActor))
+    {
+        UGameplayStatics::OpenLevel(GetWorld(), DestinationLevel, true);
+    }
+}
