ce322ec4114ec57efebf28543fcfc68046422b74
chentx06 <chentx06@gmail.com>
2025-06-19 16:34:29 -0400

    Update BackroomsLevel1.cpp

--- BackroomsLevel1.cpp
+++ BackroomsLevel1.cpp
@@ -1,1 +1,28 @@
 
+#include "BackroomsLevel1.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+ABackroomsLevel1::ABackroomsLevel1()
+{
+    PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABackroomsLevel1::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (ObjectiveWidgetClass)
+    {
+        UUserWidget* ObjectiveWidget = CreateWidget<UUserWidget>(
+            GetWorld(),
+            ObjectiveWidgetClass,
+            TEXT("ObjectiveWidget")
+        );
+        
+        if (ObjectiveWidget)
+        {
+            ObjectiveWidget->AddToViewport();
+        }
+    }
+}
