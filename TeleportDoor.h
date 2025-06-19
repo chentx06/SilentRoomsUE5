07abc2da4454d98ca36b7b214fdd4c96d8f8da2f
chentx06 <chentx06@gmail.com>
2025-06-19 16:45:32 -0400

    Create TeleportDoor.h

---
+++ TeleportDoor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "teleportdoor.generated.h"
+
+class UBoxComponent;
+class ABP_FirstPersonCharacter;
+
+UCLASS()
+class YOURGAME_API Ateleportdoor : public AActor
+{
+    GENERATED_BODY()
+    
+public:
+    Ateleportdoor();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UBoxComponent* TeleportTrigger;
+
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UPROPERTY(EditAnywhere, Category = "Teleport")
+    FName DestinationLevel = "BackroomsLevel1";
+};
