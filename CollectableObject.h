a327e4f3c6453d083897a832c08d82824a5f0074
chentx06 <chentx06@gmail.com>
2025-06-19 17:04:26 -0400

    Update CollectableObject.h

--- CollectableObject.h
+++ CollectableObject.h
@@ -11,21 +11,20 @@ class YOURPROJECT_API ACollectableObject : public AActor
     
 public:    
     ACollectableObject();
-
 protected:
     virtual void BeginPlay() override;
+
+
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     class UPrimitiveComponent* InteractionBox;
 
+
     UPROPERTY(BlueprintReadWrite)
     bool CanPickUp;
-
     UFUNCTION()
     void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
     UFUNCTION()
-    void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
+    void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent,  AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
     void OnEKeyPressed();
 };
