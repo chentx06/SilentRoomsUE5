a8b32d17df8e8b8dee36d9741cbec70452fe7352
chentx06 <chentx06@gmail.com>
2025-06-19 17:03:10 -0400

    Update ChaseAi.h

--- ChaseAi.h
+++ ChaseAi.h
@@ -1,23 +1,20 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "chaseai.generated.h"
 
 UCLASS()
-class YOURGAME_API Achaseai : public AAIController
+class YOURGAME_API Achaseai: public AAIController
 {
     GENERATED_BODY()
-
 public:
     Achaseai();
-
 protected:
     virtual void BeginPlay() override;
-
 private:
+
+
     FTimerHandle TimerHandle;
     APawn* PlayerPawn;
-
     void ChasePlayer();
 };
