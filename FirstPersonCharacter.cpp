ed854f222d5b31dd98875841957ed32f5207893e
chentx06 <chentx06@gmail.com>
2025-06-19 17:05:40 -0400

    Update FirstPersonCharacter.cpp

--- FirstPersonCharacter.cpp
+++ FirstPersonCharacter.cpp
@@ -12,7 +12,7 @@ void AFirstPersonCharacter::BeginPlay()
 {
     Super::BeginPlay();
 
-    // Add input mapping context
+    //for context
     if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
     {
         if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
@@ -24,16 +24,15 @@ void AFirstPersonCharacter::BeginPlay()
 
 void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+
+    //movements
     if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
     {
-        // Jumping
         EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AFirstPersonCharacter::OnJumpStarted);
         EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &AFirstPersonCharacter::OnJumpCompleted);
 
-        // Moving
         EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AFirstPersonCharacter::Move);
 
-        // Looking
         EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AFirstPersonCharacter::Look);
     }
 }
@@ -42,27 +41,19 @@ void AFirstPersonCharacter::OnJumpStarted()
 {
     Jump();
 }
-
 void AFirstPersonCharacter::OnJumpCompleted()
 {
     StopJumping();
 }
-
 void AFirstPersonCharacter::Move(const FInputActionValue& Value)
 {
     FVector2D MovementVector = Value.Get<FVector2D>();
-
     if (Controller != nullptr)
     {
-        // Add movement 
         const FRotator Rotation = Controller->GetControlRotation();
         const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-        // Forward/Backward direction
         const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
         AddMovementInput(ForwardDirection, MovementVector.Y);
-
-        // Right/Left direction
         const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
         AddMovementInput(RightDirection, MovementVector.X);
     }
@@ -70,11 +61,10 @@ void AFirstPersonCharacter::Move(const FInputActionValue& Value)
 
 void AFirstPersonCharacter::Look(const FInputActionValue& Value)
 {
-    FVector2D LookAxisVector = Value.Get<FVector2D>();
+    FVector2D LookAxisVector= Value.Get<FVector2D>();
 
-    if (Controller != nullptr)
+    if (Controller!= nullptr)
     {
-        // Add yaw and pitch input to controller
         AddControllerYawInput(LookAxisVector.X);
         AddControllerPitchInput(LookAxisVector.Y);
     }
@@ -83,7 +73,7 @@ void AFirstPersonCharacter::Look(const FInputActionValue& Value)
 void AFirstPersonCharacter::OnControllerChanged(APlayerController* OldController, APlayerController* NewController)
 {
     Super::OnControllerChanged(OldController, NewController);
-
+    
     if (NewController)
     {
         if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(NewController->GetLocalPlayer()))
